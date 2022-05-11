c522ee1d8fe64e176146642262dda11ff9ac8b3e
Nguyen Trien <trienvaj@gmail.com>
2022-05-11 16:08:46 +0700

    error display result

--- Covert-Currency/src/removeUnwanted/remove-unwanted.js
+++ Covert-Currency/src/removeUnwanted/remove-unwanted.js
@@ -8,17 +8,32 @@ Office.onReady((info) => {
 async function removeCharacterUnwanted(){
 let characterUnwanted = document.getElementById("inputCharacter").value;
 let arrayDataSelecteds = await ReturnArrayDataFromCells();
-console.log(arrayDataSelecteds[0]);
-arrayDataSelecteds.map(arrays =>{
-    arrays.filter(strCharacter =>{
-            strCharacter.includes(characterUnwanted)
-              ? console.log(`${strCharacter}đúng`)
-              : console.log(`${strCharacter} Sai`);
-    });
+let result= arrayDataSelecteds.map((element,i,arrParent) =>{
+  var orgArr=element.map((strCharacter,i,arr) => {
+    strCharacter.includes(characterUnwanted)
+      ? (strCharacter = recursionString(characterUnwanted, strCharacter))
+      : strCharacter;
+    arr = [strCharacter];
+    return arr;
+  },);
+  arrParent = [orgArr];
+  console.log(arrParent);
+  return arrParent;
 })
+rangeForData(result);
 }
 
-
+function characterWanted(characterUnwanted, strCharacter) {
+  strCharacter= strCharacter.replace(characterUnwanted,'');
+  strCharacter = recursionString(characterUnwanted, strCharacter)
+  return strCharacter;
+}
+function recursionString(characterUnwanted, strCharacter){
+  strCharacter= strCharacter.includes(characterUnwanted) 
+  ? strCharacter= characterWanted(characterUnwanted, strCharacter) 
+   : strCharacter;
+    return strCharacter;  
+}
 export async function ReturnArrayDataFromCells() {
   try {
     const result= await Excel.run(async (context) => {
@@ -58,7 +73,39 @@ export async function getContentInAddress(addressDetail) {
     } catch (error) {
       console.log(error);
     }
+}
+
+async function rangeForData(valuesRange) {
+  try {
+    await new Promise((resolve, reject) => {
+      Office.context.document.bindings.addFromPromptAsync(
+        Office.BindingType.Matrix,
+        { id: "currencyRange", promptText: "Select where to display the data" },
+        (result) => {
+          if (result.status === Office.AsyncResultStatus.Succeeded) {
+            resolve();
+          } else {
+            reject();
+          }
+        }
+      );
+    });
+    await Excel.run(async (context) => {
+      let binding = context.workbook.bindings.getItem("currencyRange");
+      let range = binding.getRange();
+      console.log(valuesRange);
+
+      range.load("address");
+
+      let resizeRange = range.getResizedRange(valuesRange.length - 1, valuesRange[0].length - 1);
+      resizeRange.getCell().format.horizontalAlignment = Excel.HorizontalAlignment.center;
+      resizeRange.values = valuesRange;
+      await context.sync();
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
 
 
 
